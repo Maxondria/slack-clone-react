7d8bd43e8ea295ffcd92962cbe1ba55ff3d714e8
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-15 18:51:13 +0300

    dashboard grid layout, create a side panel with user management dropdown

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
 import "./App.css";
 
-function App() {
-  return (
-    <div >
-      App
-    </div>
-  );
-}
+import ColorPanel from "./color-panel/ColorPanel";
+import SidePanel from "./side-panel/SidePanel";
+import MetaPanel from "./meta-panel/MetaPanel";
+import Messages from "./messages/Messages";
 
+const App = () => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel />
+
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
 export default App;
